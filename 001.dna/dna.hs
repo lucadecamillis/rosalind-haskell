b4e211a13326763cedae947461ec9e38aeca244d
Luca De Camillis <lucadecamillis@gmail.com>
2022-08-31 19:16:29 +0200

    001.dna

---
+++ 001.dna/dna.hs
@@ -0,0 +1,12 @@
+import System.Environment
+import Data.List
+import Data.Map
+
+-- Counting DNA Nucleotides
+countNucleotides :: String -> [(Char, Integer)]
+countNucleotides e = toList $ fromListWith (+) [(c, 1) | c <- e]
+
+main :: IO ()
+main = do
+    (input:_) <- getArgs
+    print $ countNucleotides input
