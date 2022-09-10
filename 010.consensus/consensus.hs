c063c694f16926cb63e80db91d6cfdf32ed92b1b
Luca De Camillis <lucadecamillis@gmail.com>
2022-09-10 14:23:01 +0200

    010.consensus: parse input into matrix

--- 010.consensus/consensus.hs
+++ 010.consensus/consensus.hs
@@ -1,9 +1,16 @@
 import Common
+import Data.List
+import Data.Text.Encoding
 import qualified Data.ByteString.Char8 as C
+
+parseLines :: [(C.ByteString, C.ByteString)] -> [[Char]]
+parseLines l = [ [ x | x <- y ] | y <- r ]
+    where r = [ C.unpack (snd e) | e <- l ]
 
 main :: IO ()
 main = do
     let file = "/home/luca/Desktop/consensus.txt"
     fasta <- C.readFile file
     let lines = parseFastaMultiline fasta
-    print lines
+    let matrix = parseLines lines
+    print matrix
