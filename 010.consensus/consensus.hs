db29961c6511625986853145ff46c68947c4fb89
Luca De Camillis <lucadecamillis@gmail.com>
2022-09-10 18:12:03 +0200

    010.consensus: profile

--- 010.consensus/consensus.hs
+++ 010.consensus/consensus.hs
@@ -7,10 +7,18 @@ parseLines :: [(C.ByteString, C.ByteString)] -> [[Char]]
 parseLines l = [ [ x | x <- y ] | y <- r ]
     where r = [ C.unpack (snd e) | e <- l ]
 
+computeProfile :: [[Char]] -> [(Char, [Int])]
+computeProfile m = [ (s, countOccurence s m) | s <- ['A', 'C', 'G', 'T'] ]
+
+countOccurence :: Char -> [[Char]] -> [Int]
+countOccurence s m = [ aggregate row | row <- m ]
+    where aggregate = length . filter (== s)
+
 main :: IO ()
 main = do
     let file = "/home/luca/Desktop/consensus.txt"
     fasta <- C.readFile file
     let lines = parseFastaMultiline fasta
     let matrix = parseLines lines
-    print matrix
+    let profile = computeProfile $ transpose matrix
+    print profile
