e4f566107bf3101473436c1824e706d67e004cb1
Luca De Camillis <lucadecamillis@gmail.com>
2022-09-11 12:30:28 +0200

    010.consensus: compute consensus

--- 010.consensus/consensus.hs
+++ 010.consensus/consensus.hs
@@ -1,5 +1,6 @@
 import Common
 import Data.List
+import Data.Ord
 import Data.Text.Encoding
 import qualified Data.ByteString.Char8 as C
 
@@ -16,6 +17,10 @@ countOccurence :: Char -> [[Char]] -> [Int]
 countOccurence s m = [ aggregate row | row <- m ]
     where aggregate = length . filter (== s)
 
+computeConsensus :: [[Int]] -> [Int]
+computeConsensus p = [ snd (getMax e) | e <- p ]
+    where getMax a = maximumBy (comparing fst) (zip a [0..])
+
 main :: IO ()
 main = do
     let file = "/home/luca/Desktop/consensus.txt"
@@ -23,4 +28,6 @@ main = do
     let lines = parseFastaMultiline fasta
     let matrix = parseLines lines
     let profile = computeProfile $ transpose matrix
+    let consensus = [ alphabet !! e | e <- computeConsensus $ transpose profile ]
     printMatrix profile
+    print (intersperse ' ' consensus)
