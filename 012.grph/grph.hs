9402cece2805de97dcba741db8f5ef1554ec4cfe
Luca De Camillis <lucadecamillis@gmail.com>
2022-09-18 12:19:38 +0200

    012.grph (2)

--- 012.grph/grph.hs
+++ 012.grph/grph.hs
@@ -1,9 +1,17 @@
 import Common
 
+computeAdjacencyList :: Int -> [([Char], [Char])] -> [([Char], [Char])]
+computeAdjacencyList k l = [ (fst x, fst y) | x <- l, y <-l, areAdjacent k (snd x) (snd y) ]
+    where
+        areAdjacent k x y = x /= y && lengthCheck x y && suffixEqPrefix k x y
+        lengthCheck x y = length x >= k && length y >= k
+        suffixEqPrefix k x y = drop (length x - k) x == take k y
 
 main :: IO ()
 main = do
     let input = "/home/luca/Desktop/grph.txt"
     let output = "/home/luca/Desktop/grph_res.txt"
     lines <- fastaTuples <$> readFastaMultiline input
-    print lines
+    let adjacencyList = computeAdjacencyList 3 lines
+    writeLines output [ unwords [ e1, e2 ] | (e1, e2) <- adjacencyList ]
+    print adjacencyList
