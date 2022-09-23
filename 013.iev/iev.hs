2b76f13721c815f870bf3894a965c99666dd1a26
Luca De Camillis <lucadecamillis@gmail.com>
2022-09-23 18:41:28 +0200

    013.iev (2)

--- 013.iev/iev.hs
+++ 013.iev/iev.hs
@@ -1,6 +1,5 @@
-pairProb = [1, 1, 0.5, 0.75, 0.5, 0]
+pairProb = [1, 1, 1, 0.75, 0.5, 0]
 
---nrPairs = [1, 0, 0, 1, 0, 1]
-nrPairs = [17095, 17760, 19020, 17779, 18384, 19669]
+nrPairs = [1, 0, 0, 1, 0, 1]
 
 p = sum [ n * p * 2 | (n, p) <- zip nrPairs pairProb ]
