006511350529feb5783d7d5611646ff8b1837275
Luca De Camillis <lucadecamillis@gmail.com>
2022-11-27 13:59:30 +0100

    014.lcsm (2)

--- 014.lcsm/lcsm.hs
+++ 014.lcsm/lcsm.hs
@@ -1,4 +1,5 @@
 import Common
+import Data.List
 
 sharedMotif :: Int -> [[Char]] -> Either [Char] [Char]
 sharedMotif minMatchLength lines = case lines of
@@ -10,8 +11,21 @@ sharedMotif minMatchLength lines = case lines of
 findMaxMotif :: Int -> [Char] -> [Char] -> [Char]
 findMaxMotif matchLenght line1 line2 = []
 
+findMatch :: Int -> Int -> [Char] -> [Char] -> Maybe [Char]
+findMatch startChar matchLenght line1 line2 =
+    subStr startChar matchLenght line1 >>= infixOf line2
+    where
+        infixOf line candidate = if candidate `isInfixOf` line
+            then Just candidate
+            else Nothing
+        subStr start count line = Just (take count (drop start line))
+        --subStr start count line = if start >= 0 && count + start < length line
+        --    then Just (take count (drop start line))
+        --    else Nothing
+
 main :: IO ()
 main = do
+    print (findMatch 7 1 "GACTTACA" "TAGACCA")
     let input = "/home/luca/Downloads/rosalind/014/rosalind_lcsm.txt"
     let output = "/home/luca/Downloads/rosalind/014/lcsm_res.txt"
     lines <- fastaSnd <$> readFastaLines input
