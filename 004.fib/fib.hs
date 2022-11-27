28776bfd18398a2575f5cf020b5e0d003c54c19e
Luca De Camillis <lucadecamillis@gmail.com>
2022-11-27 20:45:31 +0100

    memoizedRabbit

--- 004.fib/fib.hs
+++ 004.fib/fib.hs
@@ -3,3 +3,10 @@ rabbit n k
     | n == 1 = 1
     | n == 2 = 1
     | otherwise = rabbit (n-1) k + (k * rabbit (n-2) k)
+
+memoizedRabbit :: Int -> Int -> Int
+memoizedRabbit n k = map rab [0 ..] !! n
+   where
+    rab 0 = 0
+    rab 1 = 1
+    rab n = memoizedRabbit (n-1) k + (k * memoizedRabbit (n-2) k)
