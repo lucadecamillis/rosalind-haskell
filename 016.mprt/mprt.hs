a7356c08f1c5636d2b15d8d05beb9c7e68088fbb
Luca De Camillis <lucadecamillis@gmail.com>
2023-09-07 21:26:03 +0200

    016.mprt (2)

--- 016.mprt/mprt.hs
+++ 016.mprt/mprt.hs
@@ -1,15 +1,49 @@
 import Common
-import Network.HTTP.Request
-import Data.ByteString as B
+import Network.HTTP.Client as HTTP
+import Network.HTTP.Client.TLS as TLS
+import Network.HTTP.Request as R
+import Data.ByteString.Lazy as L
+import Data.ByteString.Char8 as C
+import qualified Codec.Binary.UTF8.Generic as U
 
-downloadProtein :: [Char] -> IO [[Char]]
+downloadViaRequest :: [Char] -> IO [Char]
+downloadViaRequest url = do
+    resp <- get url
+    let body = R.responseBody resp
+    let hh = R.responseHeaders resp
+    let l = C.length body
+    print l
+    print hh
+    let strCnt = U.toString body
+    return strCnt
+
+downloadViaHttp :: [Char] -> IO [Char]
+downloadViaHttp url = do
+    request <- parseRequest ("GET " ++ url)
+    manager <- newManager tlsManagerSettings
+    response <- httpLbs request manager
+    let body = HTTP.responseBody response
+    let hh = HTTP.responseHeaders response
+    let l = L.length body
+    print l
+    print hh
+    let strCnt = U.toString body
+    return strCnt
+
+downloadProtein :: [Char] -> IO [Char]
 downloadProtein id = do
     let url = "https://rest.uniprot.org/uniprotkb/" ++ id ++ ".fasta"
-    resp <- get url
-    let content = fastaSnd $ parseFastaLines $ responseBody resp
-    return content
+    --let url = "https://api.leancloud.cn/1.1/date"
+    --let url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/ChangeLog-6.5.2"
+    print url
+    strCnt1 <- downloadViaHttp url
+    strCnt2 <- downloadViaRequest url
+    --let content = U.toString body
+    --let content = fastaSnd $ parseFastaLines $ responseBody resp
+    return strCnt1
 
 main :: IO ()
 main = do
-    body <- downloadProtein "B5ZC00"
+    --body <- downloadProtein "B5ZC00"
+    body <- downloadProtein "P05067"
     print body
