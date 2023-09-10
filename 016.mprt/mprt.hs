aff290b22806ddfc8aab3a2b94325c7397cf62fa
Luca De Camillis <lucadecamillis@gmail.com>
2023-09-10 09:35:53 +0200

    016.mprt : found URL that works

--- 016.mprt/mprt.hs
+++ 016.mprt/mprt.hs
@@ -1,35 +1,40 @@
-import Common
-import Https
-import Data.ByteString as B
 import Codec.Binary.UTF8.Generic as U
+import Common
+import Data.ByteString as B
+import Https
 
-downloadProtein :: [Char] -> IO [Char]
+downloadProtein :: [Char] -> IO [[Char]]
 downloadProtein id = do
-    httpResult <- getViaRequest $ getFastaUrl id
-    let content = U.toString $ fst httpResult
-    --let content = fastaSnd $ parseFastaLines $ responseBody resp
-    return content
+  httpResult <- getViaRequest $ getFastaUrl id
+  --let content = U.toString $ fst httpResult
+  let content = fastaSnd $ parseFastaLines $ fst httpResult
+  return content
 
 getFastaUrl :: [Char] -> [Char]
-getFastaUrl id = "https://rest.uniprot.org/uniprotkb/" ++ id ++ ".fasta"
+--getFastaUrl id = "https://www.uniprot.org/uniprot/" ++ id ++ ".fasta"
+getFastaUrl id = "https://www.ebi.ac.uk/proteins/api/proteins/" ++ id ++ ".fasta"
 
 getDownloadPath :: [Char] -> [Char] -> [Char]
 getDownloadPath id tt = "/home/luca/Desktop/" ++ id ++ ".HS." ++ tt ++ ".fasta"
 
 diagHttps :: [Char] -> [Char] -> ([Char] -> IO (ByteString, Int)) -> IO ()
 diagHttps id tt f = do
-    httpResult <- f $ getFastaUrl id
-    print ("Http download (" ++ tt ++ "), nr bytes " ++ show (snd httpResult) ++ " writing to file")
-    let filePath = getDownloadPath id tt
-    B.writeFile filePath (fst httpResult)
+  httpResult <- f $ getFastaUrl id
+  print ("Http download (" ++ tt ++ "), nr bytes " ++ show (snd httpResult) ++ " writing to file")
+  let filePath = getDownloadPath id tt
+  B.writeFile filePath (fst httpResult)
+
+testHttpClients :: IO ()
+testHttpClients = do
+  --let id = "P05067"
+  let id = "B5ZC00"
+  diagHttps id "snoyberg" getViaSnoyberg
+  diagHttps id "request" getViaRequest
+  diagHttps id "aesiniath" getViaAesiniath
+  diagHttps id "conduit" getViaConduit
 
 main :: IO ()
 main = do
-    --let id = "P05067"
     let id = "B5ZC00"
-    diagHttps id "snoyberg" getViaSnoyberg
-    diagHttps id "request" getViaRequest
-    diagHttps id "aesiniath" getViaAesiniath
-    diagHttps id "conduit" getViaConduit
-    --body <- downloadProtein id
-    --print body
+    body <- downloadProtein id
+    print body
